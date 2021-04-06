900c93b38464833859ea6186ca34ed0433b09f93
Guillaume Riou <guillaume_riou@yahoo.fr>
2021-04-06 15:42:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,6 +77,7 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
         await discord_reaction_loading(message, False)
         await discord_reaction_fail(message)
         await channel.send("Terraria server is running and current server is using same filename. Stop server before replacing the file" % filename)
+        return
     
     await message.attachments[0].save(fp=filepath)
 
